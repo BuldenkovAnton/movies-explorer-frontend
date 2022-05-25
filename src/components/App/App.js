fa96c7e5483e2b08990794058ba1ec19ce0e0018
Anton Buldenkov <anton@buldenkov.ru>
2022-05-25 14:17:18 +0300

    add routes. add app styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import { publicRoutes } from '../../router/index';
+
 import './App.css';
 
 function App() {
-  return (
-    <>
 
-    </>
+  return (
+    <Switch>
+     {publicRoutes && publicRoutes.map(route =>
+          <Route
+              component={route.component}
+              path={route.path}
+              exact={route.exact}
+              key={route.path}
+          />
+        )
+     }
+
+    </Switch>
   );
 }
 
---
+++ src/router/index.js
@@ -0,0 +1,5 @@
+import Main from "../components/Main/Main";
+
+export const publicRoutes = [
+  {path: '/', component: Main, exact: true},
+]
