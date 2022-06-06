c4bd4f49e56fc5e8f95f188b79bb844831f5e214
Anton Buldenkov <anton@buldenkov.ru>
2022-06-06 14:03:58 +0300

    add route login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import { publicRoutes } from '../../router/index';
+import Login from "../Login/Login";
 
 import './App.css';
 
 function App() {
 
+  function loginHandler({ email, password }) {
+    console.log('Авторизация');
+  }
+
   return (
     <Switch>
-     {publicRoutes && publicRoutes.map(route =>
+       <Route path="/signin">
+         <Login onSubmit={loginHandler}/>
+       </Route>
+
+     {/* {publicRoutes && publicRoutes.map(route =>
           <Route
               component={route.component}
+              props={route.props}
               path={route.path}
               exact={route.exact}
               key={route.path}
           />
         )
-     }
+     } */}
 
     </Switch>
   );
