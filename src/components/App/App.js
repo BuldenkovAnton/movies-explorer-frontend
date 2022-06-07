bde1c2dce54447223bea96728683b3a56b7a942c
Anton Buldenkov <anton@buldenkov.ru>
2022-06-07 14:22:56 +0300

    add home route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from 'react-router-dom';
 import { publicRoutes } from '../../router/index';
 import Login from "../Login/Login";
+import Main from "../Main/Main";
 import Register from "../Register/Register";
 
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
 
   function loginHandler({ email, password }) {
+    setIsLoading(true);
     console.log('Авторизация');
+
+    setIsLoading(false);
   }
 
   function registerHandler({ name, email, password }) {
+    setIsLoading(true);
     console.log('Регистрация');
+
+    setIsLoading(false);
   }
 
   return (
     <Switch>
+
        <Route path="/signin">
-         <Login onSubmit={loginHandler}/>
+         <Login onSubmit={loginHandler} isLoading={isLoading}/>
        </Route>
 
        <Route path="/signup">
-         <Register onSubmit={registerHandler}/>
+         <Register onSubmit={registerHandler} isLoading={isLoading}/>
+       </Route>
+
+       <Route path="/">
+         <Main/>
        </Route>
 
      {/* {publicRoutes && publicRoutes.map(route =>
