961ee6ef628b64a914328b498bf6e643cfd577d3
Anton Buldenkov <anton@buldenkov.ru>
2022-06-07 14:23:13 +0300

    add validation on form register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,29 +3,87 @@ import { Link } from "react-router-dom";
 import Form from "../Form/Form";
 import Header from "../Header/Header";
 
-function Register({ onSubmit }) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function Register({ onSubmit, isLoading }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const [nameDirty, setNameDirty] = React.useState(false);
+  const [emailDirty, setEmailDirty] = React.useState(false);
+  const [passwordDirty, setPasswordDirty] = React.useState(false);
+
+  const [nameError, setNameError] = React.useState("Имя не может быть пустым");
+  const [emailError, setEmailError] = React.useState(
+    "Email не может быть пустым"
+  );
+  const [passwordError, setPasswordError] = React.useState(
+    "Пароль не может быть пустым"
+  );
 
   const changeNameHandler = useCallback((e) => {
-    setName(e.target.value);
+    const newName = e.target.value;
+    setName(newName);
+
+    if (newName.length === 0) {
+      setNameError("Имя не может быть пустым");
+    } else if (newName.length < 2) {
+      setNameError("Имя должно содержать 2 и более символов");
+    } else if (newName.length > 30) {
+      setNameError("Имя должно содержать 30 и меньше символов");
+    } else {
+      setNameError("");
+    }
   }, []);
 
   const changeEmailHandler = useCallback((e) => {
-    setEmail(e.target.value);
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+
+    const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (newEmail.length === 0) {
+      setEmailError("Email не может быть пустым");
+    } else if (!reg.test(String(newEmail).toLowerCase())) {
+      setEmailError("Email не корректный");
+    } else {
+      setEmailError("");
+    }
   }, []);
 
   const changePasswordHandler = useCallback((e) => {
-    setPassword(e.target.value);
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+
+    const reg = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (newPassword.length === 0) {
+      setPasswordError("Пароль не может быть пустым");
+    } else if (!reg.test(String(newPassword).toLowerCase())) {
+      setPasswordError(
+        "Пароль должен содержать хотя бы одну цифру, заглавную букву и специальный символ. Количество символов от 6 до 16."
+      );
+    } else {
+      setPasswordError("");
+    }
   }, []);
+
+  function blurHandler(e) {
+    switch (e.target.name) {
+      case "name":
+        setNameDirty(true);
+        break;
+      case "email":
+        setEmailDirty(true);
+        break;
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  }
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    onSubmit({name, email, password});
-  }
+    onSubmit({ name, email, password });
+  };
 
   return (
     <main className="page page__auth register">
@@ -36,24 +94,69 @@ function Register({ onSubmit }) {
         <fieldset className="form__fieldset">
           <label className="form__label">
             Имя
-            <input id="name" className="form__input" type="text" onChange={changeNameHandler}/>
+            <input
+              id="name"
+              name="name"
+              className="form__input"
+              type="text"
+              onChange={changeNameHandler}
+              onBlur={blurHandler}
+            />
+             {nameDirty && nameError && (
+              <span className="form__input-error">
+                {nameError}
+              </span>
+            )}
           </label>
 
           <label className="form__label">
             E-mail
-            <input id="email" className="form__input" type="email" onChange={changeEmailHandler}/>
+            <input
+              id="email"
+              name="email"
+              className="form__input"
+              type="email"
+              onChange={changeEmailHandler}
+              onBlur={blurHandler}
+            />
+             {emailDirty && emailError && (
+              <span className="form__input-error">
+                {emailError}
+              </span>
+            )}
           </label>
 
           <label className="form__label">
             Пароль
-            <input id="password" className="form__input" type="password" onChange={changePasswordHandler}/>
+            <input
+              id="password"
+              name="password"
+              className="form__input"
+              type="password"
+              onChange={changePasswordHandler}
+              onBlur={blurHandler}
+            />
+             {passwordDirty && passwordError && (
+              <span className="form__input-error">
+                {passwordError}
+              </span>
+            )}
           </label>
         </fieldset>
 
         <fieldset className="form__fieldset register__box-submit">
-          <button className="form__button">Зарегистрироваться</button>
+          <button
+            type="submit"
+            className="form__button"
+          >
+            {isLoading ? "Регистрация..." : "Зарегистрироваться"}
+          </button>
+
           <p className="register__signin-text">
-          Уже зарегистрированы? <Link className="register__signin-link link" to="/signin">Войти</Link>
+            Уже зарегистрированы?{" "}
+            <Link className="register__signin-link link" to="/signin">
+              Войти
+            </Link>
           </p>
         </fieldset>
       </Form>
