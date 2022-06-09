0d9d1885e2c42a569ca8d9e419cd52f82e6f4295
Anton Buldenkov <anton@buldenkov.ru>
2022-06-09 15:06:04 +0300

    add props isChecked

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function FilterCheckbox() {
+function FilterCheckbox({ isChecked }) {
   return (
     <label className="filter-checkbox">
-      <input className="filter-checkbox__input" type="checkbox" />
+      <input
+        className="filter-checkbox__input"
+        type="checkbox"
+        checked={isChecked}
+      />
       <span className="filter-checkbox__switch"></span>
       <span className="filter-checkbox__span">Короткометражки</span>
     </label>
-
   );
 }
 
