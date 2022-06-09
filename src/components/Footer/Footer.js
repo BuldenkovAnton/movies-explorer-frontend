66cb7356ef477516957e738db9d5ad7237e396a2
Anton Buldenkov <anton@buldenkov.ru>
2022-06-09 17:06:35 +0300

    refactor footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,18 +2,41 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="app__container footer">
-      <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+    <footer className="app__footer footer">
+      <p className="footer__title">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
       <p className="footer__date">© 2022</p>
-      <ul className="footer__menu">
-        <li>
-          <a href="#">Яндекс.Практикум</a>
+      <ul className="footer__menu ">
+        <li className="footer__menu-item">
+          <a
+            className="footer__menu-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://practicum.yandex.ru"
+          >
+            Яндекс.Практикум
+          </a>
         </li>
-        <li>
-          <a href="#">Яндекс.Практикум</a>
+        <li className="footer__menu-item">
+          <a
+            className="footer__menu-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/BuldenkovAnton"
+          >
+            Githib
+          </a>
         </li>
-        <li>
-          <a href="#">Яндекс.Практикум</a>
+        <li className="footer__menu-item">
+          <a
+            className="footer__menu-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://buldenkov.ru"
+          >
+            Buldenkov.ru
+          </a>
         </li>
       </ul>
     </footer>
