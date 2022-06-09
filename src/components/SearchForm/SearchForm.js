6f551d3596382bfe2d3baab470b9d4296353f504
Anton Buldenkov <anton@buldenkov.ru>
2022-06-09 17:27:14 +0300

    refactor searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import searchIcon from "../../images/search-icon.svg";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
-function SearchForm({
-  searchQuery,
-  searchIsMiniMovie,
-  onChangeSearchQuery,
-  onChangeIsMiniMovie,
-  onSubmit,
-}) {
+function SearchForm({ onSubmit }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchIsMiniMovie, setSearchIsMiniMovie] = useState(true);
+
+  const changeSearchQueryHandler = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const changeSearchIsMiniMovieHandler = (e) => {
+    console.log(e.target.checked);
+    setSearchIsMiniMovie(e.target.checked);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    onSubmit({ query: searchQuery, check: searchIsMiniMovie });
+  };
+
   return (
     <section className="search-form" aria-label="Поиск фильмов">
-      <form className="search-form__form" name="search" onSubmit={onSubmit}>
-        <fieldset className="search-form__fieldset">
+      <form
+        className="search-form__form"
+        name="search"
+        onSubmit={submitHandler}
+      >
+        <fieldset className="search-form__fieldset search-form__fieldset_input">
           <img className="search-form__icon" src={searchIcon} alt="Поиск" />
           <input
             className="search-form__input"
             type="text"
             placeholder="Фильм"
             value={searchQuery}
-            onChange={onChangeSearchQuery}
+            onChange={changeSearchQueryHandler}
           />
           <button className="search-form__submit">Найти</button>
         </fieldset>
         <span className="search-form__seperate"></span>
-        <fieldset className="search-form__fieldset">
+        <fieldset className="search-form__fieldset search-form__fieldset_switch">
           <FilterCheckbox
             title="Короткометражки"
             isChecked={searchIsMiniMovie}
-            onChange={onChangeIsMiniMovie}
+            onChange={changeSearchIsMiniMovieHandler}
           />
         </fieldset>
       </form>
