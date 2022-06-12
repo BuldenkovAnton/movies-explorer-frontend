dcb9f11e18c6850a0e078a6642c79e9be9187e71
Anton Buldenkov <anton@buldenkov.ru>
2022-06-12 13:39:46 +0300

    add 404 route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
+import Page404 from "../Page404/Page404";
 import Login from "../Login/Login";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
@@ -8,51 +9,54 @@ import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import SavedMovies from "../SavedMovies/SavedMovies";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
 
   function loginHandler({ email, password }) {
     setIsLoading(true);
-    console.log('Авторизация');
+    console.log("Авторизация");
 
     setIsLoading(false);
   }
 
   function registerHandler({ name, email, password }) {
     setIsLoading(true);
-    console.log('Регистрация');
+    console.log("Регистрация");
 
     setIsLoading(false);
   }
 
   return (
     <Switch>
+      <Route path="/" exact>
+        <Main />
+      </Route>
 
-       <Route path="/signin">
-         <Login onSubmit={loginHandler} isLoading={isLoading} />
-       </Route>
+      <Route path="/signin">
+        <Login onSubmit={loginHandler} isLoading={isLoading} />
+      </Route>
 
-       <Route path="/signup">
-         <Register onSubmit={registerHandler} isLoading={isLoading} />
-       </Route>
+      <Route path="/signup">
+        <Register onSubmit={registerHandler} isLoading={isLoading} />
+      </Route>
 
-       <Route path="/profile">
-         <Profile />
-       </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
 
-       <Route path="/movies">
-         <Movies />
-       </Route>
+      <Route path="/movies">
+        <Movies />
+      </Route>
 
-       <Route path="/saved-movies">
-         <SavedMovies />
-       </Route>
+      <Route path="/saved-movies">
+        <SavedMovies />
+      </Route>
 
-       <Route path="/">
-         <Main />
-       </Route>
+      <Route>
+        <Page404 />
+      </Route>
     </Switch>
   );
 }
