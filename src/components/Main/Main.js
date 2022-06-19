b67bf4ed7f4e609d855460726f233d7c5732e642
Anton Buldenkov <anton@buldenkov.ru>
2022-06-19 13:19:34 +0300

    change main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import AboutMe from "../AboutMe/AboutMe";
 import AboutProject from "../AboutProject/AboutProject";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Navigation from "../Navigation/Navigation";
 import Promo from "../Promo/Promo";
 import Techs from "../Techs/Techs";
 
 function Main() {
   return (
     <>
-      <Header mixClass="app__wrapper app__header">
-        <Navigation>
-          <li className="navigation__item navigation__item_page_home">
-            <Link to="/signup" className="link navigation__link navigation__link_page_home">
-              Регистрация
-            </Link>
-          </li>
-          <li className="navigation__item navigation__item_page_home">
-            <Link
-              to="/signin"
-              className="link navigation__link navigation__link_page_home navigation__link_type_button"
-            >
-              Войти
-            </Link>
-          </li>
-        </Navigation>
-      </Header>
+      <Header mixClass="app__wrapper app__header" />
       <Promo />
       <AboutProject />
       <Techs />
